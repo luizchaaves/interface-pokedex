c8362ebe76adb7e150effbbae0d1c19b63e01d35
Luiz Otavio <luizotavio0106@outlook.com>
2023-06-26 18:01:28 -0300

    application theme setting

---
+++ src/styles/styles.d.ts
@@ -0,0 +1,8 @@
+import 'styled-components';
+
+import Theme from './theme';
+export type ITheme = typeof Theme;
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ITheme {}
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+const theme = {
+  color: {
+    dark: '#212121',
+    medium: '#666666',
+    light: '#E0E0E0',
+    black: '#000000',
+    white: '#FFFFFF',
+    red: '#DC0A2D',
+  },
+  background: {
+    light: '#EFEFEF',
+  },
+};
+
+export default theme;
