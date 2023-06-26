6c5380ec3ad67e8e629f6ad4230aca673f986cb9
Luiz Otavio <luizotavio0106@outlook.com>
2023-06-26 17:25:03 -0300

    clear project default settings and creation of global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *, *::after, *::before{
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+  }
+
+  body, input, textarea{
+    font-family: 'Poppins', sans-serif;
+    font-size: 1rem;
+  }
+
+  html, body{
+    height: 100%;
+  }
+
+  p, span{
+    margin: 0px;
+    padding: 0px;
+  }
+`;
